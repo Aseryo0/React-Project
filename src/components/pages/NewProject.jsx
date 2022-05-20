3e202e46fb0c09adc0593ef02534b9a98c77652f
Aseryo0 <andrefelipeil@hotmail.com>
2022-05-20 17:50:31 -0300

    criação page formularios dos projetos

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,5 +1,15 @@
+import styles from './Newproject.module.css'
+import ProjectForm from '../Project/ProjectForm'
+
 function NewProject() {
-    return <h1>NewProject</h1>
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar seu serviço</p>
+            <p>formulario</p>
+            <ProjectForm />
+        </div>
+    )
 }
 
 export default NewProject
